50f550dcc3876c0d0362848e94cd444551d5c0a1
Tim Goloshchapov <tim.goloschapov@gmail.com>
2025-11-20 20:29:56 -0600

    comments added

---
+++ app/recordsSearch/Records.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+function Records({ patients, currentIndex, setCurrentIndex }) {
+  if (currentIndex === null) return null; // if no patient selected, show no records
+
+  const record = patients[currentIndex]; // the whole record of each patient
+  const main = record.data[0]; //the gist of each record
+
+  const nextRecord = () => {
+    //next button functionality
+    const next = (currentIndex + 1) % patients.length; // % patients.length prevents the index from ever stopping at the last patient.
+    setCurrentIndex(next); //changes currentIndex everywhere
+  };
+
+  // in real life, function below can be avoided
+  function formatToDDMMYYYY(timestamp) {
+    const date = new Date(timestamp);
+
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+  }
+
+  return (
+    <div className="border-solid border-4 border-pink-700">
+      <div className="flex">
+        <div className="mx-auto text-center border-solid border-pink-700">
+          <h4 className="text-xl">{main.userName}</h4>
+          <h5>
+            <strong>DOB:</strong> {main.userDob}
+          </h5>
+          <h5>
+            <strong>Height:</strong> {main.meta.height} cm
+          </h5>
+        </div>
+
+        <button
+          className="m-2 p-2 bg-green-600 hover:bg-green-300 hover:text-black"
+          onClick={nextRecord}
+        >
+          Next
+        </button>
+      </div>
+
+      <table className="border-solid border-t-4 border-pink-700">
+        <thead className="border-b-2 border-pink-700">
+          <tr className="">
+            <th className="border-r-2 border-pink-700 p-2">SL</th>
+            <th className="border-r-2 border-pink-700 p-2">Date</th>
+            <th className="border-r-2 border-pink-700 p-2">Diagnosis</th>
+            <th className="border-r-2 border-pink-700 p-2">Weight</th>
+            <th>Doctor</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {record.data.map((item, i) => (
+            <tr
+              className="border-b-2 border-pink-700 border-dotted"
+              key={item.id}
+            >
+              <td className="border-r-2 border-pink-700 p-2">{i + 1}</td>
+              <td className="border-r-2 border-pink-700 p-2">
+                {formatToDDMMYYYY(item.timestamp)}
+              </td>
+              <td className="border-r-2 border-pink-700 p-2">
+                {item.diagnosis.name}
+              </td>
+              <td className="border-r-2 border-pink-700 p-2">
+                {item.meta.weight}
+              </td>
+              <td className="p-2">{item.doctor.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Records;
---
+++ app/recordsSearch/Search.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+function Search({ patients, setCurrentIndex }) {
+  const [selected, setSelected] = useState("");
+
+  const handleClick = () => {
+    // you click show button. If nothing is selected than nothing really happens.
+    // if a patient from the list is selected, it changes the state of the currentIndex, declared in the main function of this app.
+    if (!selected || selected === "0") {
+      // since value of 0 is assigned to "Select Patient" option, first patient John Oliver gets value of 1
+      alert("Please select a patient name");
+      return;
+    }
+
+    // Convert patient id to index (id 1→index0, etc.)
+    // it is currently "1" "2" "3"
+    setCurrentIndex(Number(selected) - 1);
+  };
+
+  return (
+    <div className=" m-5 flex justify-center items-center">
+      <div className="flex items-center">
+        <select
+          className="bg-pink-700 w-64 p-2"
+          data-testid="patient-name"
+          defaultValue="0"
+          onChange={(e) => setSelected(e.target.value)}
+        >
+          <option value="0" disabled>
+            Select Patient
+          </option>
+
+          {patients.map((p) => (
+            <option key={p.id} value={p.id}>
+              {p.data[0].userName}
+              {/* grabs data from medicalRecords */}
+            </option>
+          ))}
+        </select>
+
+        <button
+          className="m-2 p-2 bg-green-600 hover:bg-green-300 hover:text-black"
+          type="submit"
+          data-testid="show"
+          onClick={handleClick}
+        >
+          Show
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
