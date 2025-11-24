abeee86be8fc279f26072339da7cdae2150a36e3
Tim Goloshchapov <tim.goloschapov@gmail.com>
2025-11-23 22:01:33 -0600

    basic version done

--- app/worldCupDraw/page.tsx
+++ app/worldCupDraw/page.tsx
@@ -1,123 +1,123 @@
 "use client";
 
 import Link from "next/link";
-import { useState, useEffect } from "react";
 import { MoveLeft } from "lucide-react";
+import { useState } from "react";
 
-function drawGenerator(pot1, pot2, pot3, pot4) {
-  //array of arrays?
-  //first team in each pot goes to group A
-  //all teams that wen to group A should be unavailable to pick for next groups
-  //Do this until there are no teams left/pot 1 is empty
-  //Version 1 ---> on click of the Draw button, all groups are filled with teams right away
-  //Version 2 ---> user sees teams filling the groups one by one in the order of pots (all teams from pot 1 first)
-  //Version 3 ---> user sees team filling the groups one by one in the order of groups (all teams in group A first)
+type Pots = string[][];
 
-  //get a random team from pot1
-  let handOfMaterazzi = Math.floor(Math.random() * 8); //random grab // needs to be called on EVERY team
-  let teamOne = pot1[handOfMaterazzi];
-  let teamTwo = pot2[handOfMaterazzi];
-  let teamThree = pot3[handOfMaterazzi];
-  let teamFour = pot4[handOfMaterazzi];
+function drawGenerator(pots: Pots) {
+  const potsCopy = pots.map((pot) => [...pot]); // made a copy, so we dont mutate the original pots
 
-  let group = [teamOne, teamTwo, teamThree, teamFour];
-  return group;
+  const groups: string[][] = Array.from({ length: 12 }, () => []);
+
+  for (let i = 0; i < groups.length; i++) {
+    for (let j = 0; j < potsCopy.length; j++) {
+      const hand = Math.floor(Math.random() * potsCopy[j].length);
+      groups[i].push(potsCopy[j][hand]);
+      potsCopy[j].splice(hand, 1);
+    }
+  }
+  return groups;
 }
 
-const Groups = (props) => {
+type Props = {
+  teams: string[][];
+};
+
+const Groups = (props: Props) => {
   return (
     <div className="flex m-5">
-      <div className="bg-green-600 p-3">
-        <p className="bg-pink-600 text-2xl">Group A</p>
-        {props.teams.map((team: string) => {
-          return <p className="text-xl">{team}</p>;
-        })}
-      </div>
-      {/* map the amount of groups based on the amount of teams in the draw */}
-      <div className="m-2 bg-green-600">Group B</div>
-      <div className="m-2 bg-green-600">Group C</div>
-      <div className="m-2 bg-green-600">Group D</div>
-      <div className="m-2 bg-green-600">Group E</div>
-      <div className="m-2 bg-green-600">Group F</div>
-      <div className="m-2 bg-green-600">Group G</div>
-      <div className="m-2 bg-green-600">Group H</div>
-      <div className="m-2 bg-green-600">Group I</div>
-      <div className="m-2 bg-green-600">Group J</div>
-      <div className="m-2 bg-green-600">Group K</div>
-      <div className="m-2 bg-green-600">Group L</div>
+      {props.teams.map((group: string[], index: number) => (
+        <div key={index} className="m-3">
+          <p className="text-xl bg-pink-600 p-3">Group {index + 1} </p>
+          {group.map((team, i) => (
+            <p key={i} className="bg-green-700 p-2">
+              {team}
+            </p>
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
 
 export default function NextComponent() {
-  // itd be cool to grab the teams from fifa website instead of hard coding
-  const potOne = [
-    "USA",
-    "Mexico",
-    "Canada",
-    "Spain",
-    "Argentina",
-    "France",
-    "England",
-    "Brazil",
-    "Portugal",
-    "Netherlands",
-    "Belgium",
-    "Germany",
-  ];
-  const potTwo = [
-    "Croatia",
-    "Morocco",
-    "Colombia",
-    "Uruguay",
-    "Switzerland",
-    "Japan",
-    "Senegal",
-    "Iran",
-    "South Korea",
-    "Ecuador",
-    "Austria",
-    "Australia",
-  ];
-  const potThree = [
-    "Norway",
-    "Panama",
-    "Egypt",
-    "Algeria",
-    "Scotland",
-    "Paraguay",
-    "Tunisia",
-    "Ivory Coast",
-    "Uzbekistan",
-    "Qatar",
-    "Saudi Arabia",
-    "South Africa",
-  ];
-  const potFour = [
-    "Jordan",
-    "Cape-Verde",
-    "Ghana",
-    "Curacao",
-    "Haiti",
-    "New Zealand",
-    "UEFA A",
-    "UEFA B",
-    "UEFA C",
-    "UEFA D",
-    "INTER-CON A",
-    "INTER-CON B",
+  const pots = [
+    [
+      "USA",
+      "Mexico",
+      "Canada",
+      "Spain",
+      "Argentina",
+      "France",
+      "England",
+      "Brazil",
+      "Portugal",
+      "Netherlands",
+      "Belgium",
+      "Germany",
+    ],
+    [
+      "Croatia",
+      "Morocco",
+      "Colombia",
+      "Uruguay",
+      "Switzerland",
+      "Japan",
+      "Senegal",
+      "Iran",
+      "South Korea",
+      "Ecuador",
+      "Austria",
+      "Australia",
+    ],
+    [
+      "Norway",
+      "Panama",
+      "Egypt",
+      "Algeria",
+      "Scotland",
+      "Paraguay",
+      "Tunisia",
+      "Ivory Coast",
+      "Uzbekistan",
+      "Qatar",
+      "Saudi Arabia",
+      "South Africa",
+    ],
+    [
+      "Jordan",
+      "Cape-Verde",
+      "Ghana",
+      "Curacao",
+      "Haiti",
+      "New Zealand",
+      "UEFA A",
+      "UEFA B",
+      "UEFA C",
+      "UEFA D",
+      "INTER-CON A",
+      "INTER-CON B",
+    ],
   ];
 
-  //const result = ["Norway", "Panama", "Egypt", "Algeria"];
-  const result = drawGenerator(potOne, potTwo, potThree, potFour);
-  //call the draw generator
+  const [result, setResult] = useState<string[][]>([]);
+
+  const drawGroups = () => {
+    setResult(drawGenerator(pots));
+  };
+
   return (
     <div className="flex flex-col items-center justify-center">
       <Link href="/">
         <MoveLeft className="m-5" />
       </Link>
       <header className="text-5xl text-cyan-300 mt-5">World Cup Draw UI</header>
-      <button className="bg-blue-500 p-3 m-5 hover:bg-blue-400">
+      <button
+        onClick={drawGroups}
+        className="bg-blue-500 p-3 m-5 hover:bg-blue-400"
+      >
         Hand of Materazzi
       </button>
       <Groups teams={result} />
