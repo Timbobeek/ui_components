246e627fbb94eedd947b94f2fb372063cd885906
Tim Goloshchapov <tim.goloschapov@gmail.com>
2025-12-07 01:14:10 -0600

    vertical dragging only

--- app/draggableList2/page.tsx
+++ app/draggableList2/page.tsx
@@ -11,6 +11,10 @@ import { CSS } from "@dnd-kit/utilities";
 import { useState } from "react";
 import Link from "next/link";
 import { MoveLeft } from "lucide-react";
+import {
+  restrictToParentElement,
+  restrictToVerticalAxis,
+} from "@dnd-kit/modifiers";
 
 type Item = { id: string; label: string; background: string };
 
@@ -29,7 +33,7 @@ const SortableItem = ({ id, label, background }: Item) => {
       style={style}
       {...attributes}
       {...listeners}
-      className={`${background} p-3 mb-2 shadow rounded cursor-grab active:cursor-grabbing select-none`}
+      className={`${background} p-3 shadow rounded cursor-grab active:cursor-grabbing select-none`} // can return margins later if needed
     >
       {label}
     </div>
@@ -63,10 +67,11 @@ export default function SortableList() {
         Draggable List v2
       </header>
       <p className=" text-indigo-300 ">dnd-kit library</p>
-      <div className="p-4 w-1/4 mt-8">
+      <div className=" w-1/4 mt-8">
         <DndContext
           collisionDetection={closestCenter}
           onDragEnd={handleDragEnd}
+          modifiers={[restrictToParentElement, restrictToVerticalAxis]} //adds verticla dragging limitation
         >
           <SortableContext
             items={items.map((i) => i.id)}
@@ -77,6 +82,28 @@ export default function SortableList() {
             ))}
           </SortableContext>
         </DndContext>
+      </div>
+      <div className="flex w-1/2 ">
+        <div className="m-5 bg-pink-900 p-3 border-4 border-white w-1/2 ">
+          <p className="text-2xl m-5 text-center text-pink-300">
+            Challening parts{" "}
+          </p>
+          <p className="">
+            Documentation of dnd-kit library was outdated, some imports were not
+            going through as modules from the docs did not exits.
+          </p>
+        </div>
+        <div className="m-5 bg-lime-900 p-3 border-4 border-white w-1/2 ">
+          <p className="text-2xl m-5 text-center text-lime-300">
+            Things I learned here{" "}
+          </p>
+          <p>
+            Unrelated, but to use multiple cursors in VS Code. I forgot the
+            shortcut once again. It is ctrl + alt + arrow down on Windows. And
+            the hold ctrl and press right arrow if you want to get to the end of
+            each line and start typing there.
+          </p>
+        </div>
       </div>
     </div>
   );
