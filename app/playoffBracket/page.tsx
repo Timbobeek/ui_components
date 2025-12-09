b8c319dcf0bc467f96f3a18ae352aab03a7557e9
Tim Goloshchapov <tim.goloschapov@gmail.com>
2025-12-08 19:46:47 -0600

    global handleClick working

--- app/playoffBracket/page.tsx
+++ app/playoffBracket/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useState } from "react";
 import { MoveLeft } from "lucide-react";
 import { teams } from "./teams";
+import { match } from "assert";
 
 export default function NextComponent() {
   const [contenders, setContenders] = useState(teams);
@@ -38,6 +39,37 @@ export default function NextComponent() {
     });
   };
 
+  //prototype
+  const handleClick = ({
+    matchID,
+    teamID,
+  }: {
+    matchID: number;
+    teamID: number;
+  }) => {
+    // if there is more than one team that has selected = true
+    if (contenders[matchID].find((team) => team.selected)) {
+      const otherTeamID = contenders[matchID].findIndex(
+        (team) => team.selected
+      );
+      setContenders((prev) => {
+        //unselect that other team
+        const copy = prev.map((group) => [...group]);
+        copy[matchID][otherTeamID] = {
+          ...copy[matchID][otherTeamID],
+          selected: false,
+        };
+        return copy;
+      });
+    }
+    setContenders((prev) => {
+      //select current team
+      const copy = prev.map((group) => [...group]);
+      copy[matchID][teamID] = { ...copy[matchID][teamID], selected: true };
+      return copy;
+    });
+  };
+
   return (
     <div className="flex flex-col items-center justify-center">
       <Link href="/">
@@ -51,37 +83,37 @@ export default function NextComponent() {
               className={` text-black p-5 m-3 hover:bg-yellow-300 ${
                 contenders[0][0].selected ? "bg-yellow-300" : "bg-green-500"
               }`}
-              onClick={handleClick1}
+              onClick={() => handleClick({ matchID: 0, teamID: 0 })} // pass matchID and teamID
             >
-              Brazil
+              {contenders[0][0].label}
             </button>
             <button
               className={` text-black p-5 m-3 hover:bg-yellow-300 ${
                 contenders[0][1].selected ? "bg-yellow-300" : "bg-green-500"
               }`}
-              onClick={handleClick2}
+              onClick={() => handleClick({ matchID: 0, teamID: 1 })}
             >
-              Argentina
+              {contenders[0][1].label}
             </button>
           </div>
-          {/* <div className="flex flex-col">
+          <div className="flex flex-col">
             <button
               className={` text-black p-5 m-3 hover:bg-yellow-300 ${
-                selected3 ? "bg-yellow-300" : "bg-green-500"
+                contenders[1][0].selected ? "bg-yellow-300" : "bg-green-500"
               }`}
-              onClick={handleClick}
+              onClick={() => handleClick({ matchID: 1, teamID: 0 })} // pass matchID and teamID
             >
-              Spain
+              {contenders[1][0].label}
             </button>
             <button
               className={` text-black p-5 m-3 hover:bg-yellow-300 ${
-                selected4 ? "bg-yellow-300" : "bg-green-500"
+                contenders[1][1].selected ? "bg-yellow-300" : "bg-green-500"
               }`}
-              onClick={handleClick}
+              onClick={() => handleClick({ matchID: 1, teamID: 1 })}
             >
-              Germany
+              {contenders[1][1].label}
             </button>
-          </div> */}
+          </div>
         </div>
         {/* -----------------------next stage-------------------------- */}
         {/* <div className="flex flex-col w-1/4 justify-center">
@@ -109,6 +141,31 @@ export default function NextComponent() {
             <p className="text-3xl">Brazil</p>
           </div>
         </div> */}
+      </div>
+      <div className="flex w-1/2 ">
+        <div className="m-5 bg-pink-900 p-3 border-4 border-white w-1/2 ">
+          <p className="text-2xl m-5 text-center text-pink-300">
+            Challening parts{" "}
+          </p>
+          <p className="">
+            A lot of states. I have an overall state of all teams present at the
+            beginning fo the tournament. Then the user has to choose one of the
+            two teams in each match-up. The chosen team's state transfers to the
+            next stage of the bracket. Finding the ideal type to keep the teams
+            in for all of the future manipulations is important. I want to avoid
+            having 60 handleClick functions.
+          </p>
+        </div>
+        <div className="m-5 bg-lime-900 p-3 border-4 border-white w-1/2 ">
+          <p className="text-2xl m-5 text-center text-lime-300">
+            Things I learned here{" "}
+          </p>
+          <p>
+            One must pass the function to be executed later into onClick, not
+            call the function immediately.
+            {/* Correct is onClick={() => handleClick(stuff)} , incorrect is onClick={handleClick} */}
+          </p>
+        </div>
       </div>
     </div>
   );
--- app/playoffBracket/teams.tsx
+++ app/playoffBracket/teams.tsx
@@ -4,10 +4,14 @@ export type TeamsProps = {
   teams: Team[];
 };
 
+//option 1 -- split by pairs  [ [{},{}] , [{},{}] ]
+
 export const teams = [
   [
     { id: "1", label: "Brazil", selected: false, background: "bg-green-500" },
     { id: "2", label: "Canada", selected: false, background: "bg-red-500" },
+  ],
+  [
     {
       id: "3",
       label: "Ivory Coast",
@@ -24,7 +28,32 @@ export const teams = [
       background: "bg-green-500",
     },
     { id: "2", label: "Mexico", selected: false, background: "bg-red-500" },
+  ],
+  [
     { id: "3", label: "Ghana", selected: false, background: "bg-yellow-500" },
     { id: "4", label: "Poland", selected: false, background: "bg-blue-500" },
   ],
 ];
+
+//option 2 -- all in one  [{},{},{}]
+
+// export const teams = [
+//   { id: "1", label: "Brazil", selected: false, background: "bg-green-500" },
+//   { id: "2", label: "Canada", selected: false, background: "bg-red-500" },
+//   {
+//     id: "3",
+//     label: "Ivory Coast",
+//     selected: false,
+//     background: "bg-yellow-500",
+//   },
+//   { id: "4", label: "Iceland", selected: false, background: "bg-blue-500" },
+//   {
+//     id: "1",
+//     label: "Argentina",
+//     selected: false,
+//     background: "bg-green-500",
+//   },
+//   { id: "2", label: "Mexico", selected: false, background: "bg-red-500" },
+//   { id: "3", label: "Ghana", selected: false, background: "bg-yellow-500" },
+//   { id: "4", label: "Poland", selected: false, background: "bg-blue-500" },
+// ];
